31e7ac97cbf3538ba489f5f9e8b67544dc2e4c1f
R-Rangel <r.rangel.0222@gmail.com>
2023-03-09 13:51:31 -0600

    pulling datatset

--- server.js
+++ server.js
@@ -9,19 +9,19 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-      maxAge: 24 * 60 * 60 * 1000, 
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize,
-    }),
-  };
+// const sess = {
+//     secret: 'Super secret secret',
+//     cookie: {
+//       maxAge: 24 * 60 * 60 * 1000, 
+//     },
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//       db: sequelize,
+//     }),
+//   };
   
-  app.use(session(sess));
+//   app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
