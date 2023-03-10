6ff754c5109955f1a6c9387e85b16252a8a876ea
Awele1111 <awelelan@Aweles-MacBook-Air.local>
2023-03-10 14:51:49 -0600

    added new files

--- public/js/login.js
+++ public/js/login.js
@@ -1,33 +1,76 @@
 
-const usernameInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-const loginButton = document.getElementById("login");
+// const usernameInput = document.getElementById("email");
+// const passwordInput = document.getElementById("password");
+// const loginButton = document.getElementById("login");
 
-function handleLogin() {
-  const email = emailInput.value;
-  const password = passwordInput.value;
+// function handleLogin() {
+//   const email = emailInput.value;
+//   const password = passwordInput.value;
 
 
-  fetch("/login", {
-    method: "POST",
-    body: JSON.stringify({ email, password }),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Failed to login");
-    }
-    return response.json();
+//   fetch("/login", {
+//     method: "POST",
+//     body: JSON.stringify({ email, password }),
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   })
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error("Failed to login");
+//     }
+//     return response.json();
 
-  })
-  .catch(error => {
-    console.error(error);
+//   })
+//   .catch(error => {
+//     console.error(error);
    
-    alert(error.message);
-  });
-}
+//     alert(error.message);
+//   });
+// }
 
 
-LoginButton.addEventListener("click", handleLogin);
+// LoginButton.addEventListener("click", handleLogin);
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,26 +1,41 @@
-const logoutButton = document.getElementById("logout");
+// const logoutButton = document.getElementById("logout");
 
-function handleLogout() {
-    localStorage.removeItem("token");
-    window.location.replace("/login");
+// function handleLogout() {
+//     localStorage.removeItem("token");
+//     window.location.replace("/login");
 
 
-  fetch("/logout", {
-    method: "POST"
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Failed to logout");
-    }
+//   fetch("/logout", {
+//     method: "POST"
+//   })
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error("Failed to logout");
+//     }
 
-    return response.json();
-  })
-  .catch(error => {
-    console.error(error);
-    window.location.replace("/login");
+//     return response.json();
+//   })
+//   .catch(error => {
+//     console.error(error);
+//     window.location.replace("/login");
+//   });
+// }
+
+// logoutButton.addEventListener("click", handleLogout);
+
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
   });
-}
 
-logoutButton.addEventListener("click", handleLogout);
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
 
 
